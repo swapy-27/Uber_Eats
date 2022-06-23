319a7b2744ea5dc5cd12d3e8dbaf779d616805f9
Swapnil <swapniljain240@gmail.com>
2022-06-23 14:07:53 +0530

    categories component

---
+++ uber_eats/components/SearchBar.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react';
+
+// import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+export default function SearchBar() {
+  return (
+    <View>
+      {/* <GooglePlacesAutocomplete/> */}
+      <Text>SearchBar</Text>
+    </View>
+  )
+}
--- uber_eats/screens/Home.js
+++ uber_eats/screens/Home.js
@@ -1,12 +1,21 @@
 import { View, Text, SafeAreaView, SafeAreaViewBase } from 'react-native'
 import React from 'react'
 import HeaderTab from '../components/HeaderTab'
+import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
 
 export default function Home() {
   return (
-    <SafeAreaView>
-        <HeaderTab/>
-    </SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
+      <View style={{ backgroundColor: 'white', padding: 15 }}>
+        <HeaderTab />
+        <SearchBar />
+      </View>
+      
+        <Categories />
     
+
+    </SafeAreaView>
+
   )
 }
